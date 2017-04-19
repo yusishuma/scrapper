3ba95817a5f9747c49704f1c9ef5e3cc7d4b772b
tonghe ma <1179610268@qq.com>
2017-04-19 16:44:04 +0800

    fixed；test port

--- public/api_project.js
+++ public/api_project.js
@@ -2,9 +2,9 @@ define({
   "name": "fluorescence",
   "version": "1.0.0",
   "title": "fluorescence apiDoc",
-  "url": "http://192.168.2.29:3000/api",
+  "url": "http://192.168.2.29:3090/api",
   "generator": {
     "version": "0.7.2",
-    "time": "2017-04-19T08:37:25.143Z"
+    "time": "2017-04-19T08:43:39.303Z"
   }
 });
